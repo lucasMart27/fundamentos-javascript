0b61c705531559885ab67e6eaaa2dd404ccd791f
lucasMart27 <lucas_mart27@hotmail.com>
2022-11-08 14:18:02 -0300

    Exemplos

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+const numero1 = 5;
+const numero2 = 5;
+let cadastroAtivado = true;
+
+console.log(numero1 === numero2);
+
+
+const texto1 = "teste";
+const texto2 = "Teste";
+
+const texto2Minusculo = texto2.toLowerCase();
+
+
+console.log(texto1 === texto2Minusculo);
---
+++ type-string.js
@@ -0,0 +1,30 @@
+const texto1 = "Olá, mundo!";
+
+console.log(texto1);
+
+const meuNome ="Meu nome é ";
+const nome ="Lucas Martins";
+
+console.log(meuNome + nome);
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
+
+
+
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
